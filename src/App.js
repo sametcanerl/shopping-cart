d0aaf9f4e0437b4a13efe4efc7fb20707ca87055
sametcanerl <sametcaneol28@gmail.com>
2022-08-20 23:57:40 +0300

    Product Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Home from "./pages/home/Home";
+import Product from "./pages/product/Product";
 import ProductList from "./pages/productlist/ProductList";
-
 
 function App() {
   return (
-  // <Home/>
-  <ProductList/>
-  )
+    // <Home/>
+    // <ProductList/>
+    <Product />
+  );
 }
 
 export default App;
-
-
